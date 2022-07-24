f3f0094931acd4133d903e8d98f2373088370985
gaowei <huyao9950@hotmail.com>
2022-07-24 16:45:21 +0800

    opt: 优化js

--- web/assets/js/theme.js
+++ web/assets/js/theme.js
@@ -6,7 +6,7 @@
     var $book = $(".book");
     setTimeout(function () {
         $('.toggle-summary').on('click', function () {
-            $book.toggleClass('with-summary', $book.hasClass("with-summary"));
+            $book.toggleClass('with-summary', !$book.hasClass("with-summary"));
         });
     }, 1);
 })();
